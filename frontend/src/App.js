76b8b84b13512caf807b973ba0dcd7360bebed83
penghuidong <penghuidong520@gmail.com>
2022-10-24 11:24:05 -0400

    setup frontend and backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,9 @@
 
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { Switch } from 'react-router-dom';
-
-import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
+import { Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
-
 import MainPage from './components/MainPage/MainPage';
-import LoginForm from './components/SessionForms/LoginForm';
-import SignupForm from './components/SessionForms/SignupForm';
-import Tweets from './components/Tweets/Tweets';
-import Profile from './components/Profile/Profile';
-import TweetCompose from './components/Tweets/TweetCompose';
-
 import { getCurrentUser } from './store/session';
 
 function App() {
@@ -26,13 +17,7 @@ function App() {
     <>
       <NavBar />
       <Switch>
-        <AuthRoute exact path="/" component={MainPage} />
-        <AuthRoute exact path="/login" component={LoginForm} />
-        <AuthRoute exact path="/signup" component={SignupForm} />
-
-        <ProtectedRoute exact path="/tweets" component={Tweets} />
-        <ProtectedRoute exact path="/profile" component={Profile} />
-        <ProtectedRoute exact path="/tweets/new" component={TweetCompose} />
+        <Route exact to="/" component={MainPage} />
       </Switch>
     </>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import configureStore from './store/store';
 
-let store = configureStore({});
+const rootElement = document.getElementById('root')
+const root = ReactDOM.createRoot(rootElement);
+
+const store = configureStore({});
 
 function Root() {
   return (
@@ -18,9 +21,20 @@ function Root() {
   );
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const renderApplication = () => {
+	root.render(
+		<React.StrictMode>
+			<Root />
+		</React.StrictMode>
+	);
+}
+
+renderApplication();
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Root />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
