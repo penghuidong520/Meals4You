c0563c38e09f7410165c20e7017ae89c0628e9c0
penghuidong <penghuidong520@gmail.com>
2022-10-25 17:57:51 -0400

    update profile dishes functionality, able to add and delete

--- frontend/src/components/DishIndex/DishItem.js
+++ frontend/src/components/DishIndex/DishItem.js
@@ -1,10 +1,19 @@
+import { useDispatch } from "react-redux";
+import { deleteDish } from "../../store/dishes";
 
 const DishItem = ({dish}) => {
+    const dispatch = useDispatch();
+
+    const handleDelete = e => {
+        // e.preventDefault();
+        dispatch(deleteDish(dish._id));
+    }
 
     return (
-        <>
+        <div className="dish-item">
             <h1>{dish.name}</h1>
-        </>
+            <button className="dish-item-delete-button" onClick={handleDelete} >Delete Dish</button>
+        </div>
     )
 }
 
