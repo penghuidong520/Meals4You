3719a83e6d0ab212ff66fad39469bf2384ae126d
penghuidong <penghuidong520@gmail.com>
2022-10-25 11:17:09 -0400

    connect frontend and backend sessions, able to persist state after refresh

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,29 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo  from '../../images/logo-meals4u.png';
 import './Header.css';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux'
+import { login, logout } from '../../store/session';
 
 function Header() {
-  return (
-    <div className='header'>
-        <div className='header-logo-container'>
-                <Link to='/' >
-                    <img id="header-logo" src={logo} alt="" />
-                </Link>
-        </div>
-        <div className='header-button-container'>
-            <Link className='header-button' to='/login'>
-                Log In
-            </Link>
-            <Link className='header-button' to='/signup'>
-                Sign Up
-            </Link>
-            <div className='header-button'> Demo User
-                {/* <button to='/'>Demo User</button> */}
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user)
+
+    useEffect(()=>{
+
+    }, [sessionUser]);
+
+    const handleLogout = (e) => {
+        e.preventDefault();
+        dispatch(logout());
+    }
+
+    const handleDemo = (e) => {
+        e.preventDefault();
+        dispatch(login( { email: "demo@user.io", password: "password" } ))
+    }
+
+    return (
+        <div className='header'>
+            <div className='header-logo-container'>
+                    <Link to='/' >
+                        <img id="header-logo" src={logo} alt="" />
+                    </Link>
+            </div>
+            <div className='header-button-container'>
+                {!sessionUser && <Link className='header-button' to='/login'>
+                    Log In
+                </Link>}
+                {!sessionUser && <Link className='header-button' to='/signup'>
+                    Sign Up
+                </Link>}
+                {sessionUser && <button className='header-button' onClick={handleLogout}>
+                    Log Out
+                </button>}
+                <button className='header-button' onClick={handleDemo} > Demo User
+                    {/* <button to='/'>Demo User</button> */}
+                </button>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default Header;
