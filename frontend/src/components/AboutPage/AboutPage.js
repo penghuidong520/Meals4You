849863a654311b07cbf317b82f8008cac72fbc6a
penghuidong <penghuidong520@gmail.com>
2022-10-26 14:34:43 -0400

    update about page

---
+++ frontend/src/components/AboutPage/AboutPage.js
@@ -0,0 +1,44 @@
+
+const AboutPage = () => {
+    return (
+        <div className="about-container">
+            <div className="about-instruction">
+                <h1 className="about-title" >Instruction</h1>
+                <p>Meal 4 You is an app that have a spinning board and chooses the meal off of default selections and randomly choose a meal for the users, so users doesn't have to struggle and choose what to eat for lunch/dinner.</p>
+                <ul>How to Use
+                    <li>Landing Page has a default wheel for user to click play with</li>
+                    <li>Profile has user options to choose a saved wheel or add additional item onto the wheel</li>
+                    <li>You could create a customized wheel and save it to your list !</li>
+                </ul>
+            </div>
+            <div className="about-incentives" >
+                <h1 className="about-title" > Incentives </h1>
+                <p>How we came up with the Idea !</p>
+            </div>
+            
+            <div className="about-developers-container">
+                <h1 className="about-title" >Developers</h1>
+                <div className="about-developers">
+                    <div className="about-payton" >
+            
+                    </div>
+
+                    <div className="about-ronny" >
+
+                    </div>
+
+                    <div className="about-ivy" >
+
+                    </div>
+
+                    <div className="about-yanxi" >
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default AboutPage;
