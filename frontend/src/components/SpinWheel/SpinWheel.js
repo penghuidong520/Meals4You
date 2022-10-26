070abdaf96f2044cc19ff7f7ed1f95ded2750dff
Yanxi_Lin <yanxilinnn@gmail.com>
2022-10-26 14:02:30 -0400

    complete main page instruction skeleton

--- frontend/src/components/SpinWheel/SpinWheel.js
+++ frontend/src/components/SpinWheel/SpinWheel.js
@@ -2,14 +2,16 @@ import './SpinWheel.css';
 import { useState, useRef } from 'react';
 import React from 'react';
 import { useSelector } from 'react-redux'
+import welcomeInfo from '../../images/welcome-info.png';
+import introGif from '../../images/intro-gif.gif';
 
 
 const SpinWheel = () => {
     const sessionUser = useSelector(state => state.session.user)
+    console.log(sessionUser)
 
 
-
-  const items = ["Peter", "Paython", "Yanxi", "Ivy", "Ronny",  "Kin", "Acye"];  
+  const items = ["Pizza", "Halal", "Bagel", "Chicken Over Rice", "Sandwich",  "Ramen", "Dumpling"];  
 
   const [selectedItem, setSelectedItem] = useState(null);
   const wheelRef = useRef();
@@ -19,21 +21,21 @@ const SpinWheel = () => {
       setSelectedItem(Math.floor(Math.random() * items.length))
     }
     
-    
     if (wheelRef.current) {
       wheelRef.current.removeEventListener('click', selectItem);
     }
   }
 
-
   const handleReset = e => {
-    if(sessionUser){
       wheelRef.current.addEventListener('click', selectItem);
       setSelectedItem(null);
-    }else{
-      window.location.assign("/login")
-    }
+    // else{
+    //   window.location.assign("/login")
+    // }
   };
+
+
+  const welcomeMessage = (sessionUser) ? 'Saved wheel' :'Click the SpinWheel \r\n Choose Meals for You';
   
   const wheelVars = {
     "--nb-item": items.length,
@@ -42,10 +44,18 @@ const SpinWheel = () => {
 
   const spinning = selectedItem !== null ? "spinning" : "";
   
-
   return (
     <div className='spin-container'>
-    <div className='spinwheel-box'>
+        <div className='left-column'>
+          <div className='intro1'>{welcomeMessage}</div>
+          {sessionUser == null && (
+            <div className='welcomeInfo-icon'>
+              <img id='welcomeInfo-icon' src={welcomeInfo} alt="" />
+              < img id='intro-gif' src={introGif} alt="" />
+            </div>
+             )}
+        </div> 
+    <div className='spinwheel-box1'>
       <div className="wheel-container">
         <div
           className={`wheel ${spinning}`}
@@ -64,16 +74,15 @@ const SpinWheel = () => {
           ))}
         </div>
       </div>
-    </div>
-    <div className='reset-containor'>
+    <div className='reset-containor1'>
       {selectedItem !== null && (
-        <div className='reset-button'>
-          <button id="reset-button" onClick={handleReset}>
-            <p id="text1" >Don't Like {items[selectedItem]}?</p>
-            <p id="text2">Click Me Reset</p>
+        <div className='reset-button1'>
+          <button id="reset-button1" onClick={handleReset}>
+            <p id="reset-text1">Reset</p>
           </button>
         </div>
       )}
+      </div>
       </div>
     </div>
   );
