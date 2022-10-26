22c27503fbcf87c6de60ae681e2383f30639454c
penghuidong <penghuidong520@gmail.com>
2022-10-26 17:24:19 -0400

    update saved wheels to populate the wheel

--- frontend/src/store/contents.js
+++ frontend/src/store/contents.js
@@ -6,19 +6,20 @@ export const updateContents = wheel => ({
 })
 
 const defaultContents = {
-    wheel: {
+    
         title: "Default Wheel",
         contents: ["Payton", "Ronny", "Yanxi", "Ivy", "Peter", "Kin", "Ayce"]
-    }
+    
 }
 
 const contentsReducer = (state = defaultContents, action) => {
-    // Object.freeze(state);
-    // const nextState = {...state};
+    Object.freeze(state);
+    const nextState = {...state};
     switch(action.type) {
         case UPDATE_CONTENTS:
             // state.contents = action.contents;
-            return action.wheel
+            
+            return {...nextState, ...action.wheel}
         default:
             return state;
     }
