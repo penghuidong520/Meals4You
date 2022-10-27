1cb465191c8421fe87220a1d86955fdb1c3b4064
Yanxi_Lin <yanxilinnn@gmail.com>
2022-10-27 00:49:54 -0400

    complete About page inital design

--- frontend/src/components/AboutPage/AboutPage.js
+++ frontend/src/components/AboutPage/AboutPage.js
@@ -2,16 +2,70 @@
 const AboutPage = () => {
     return (
         <div className="about-container">
-            <div className="about-instruction">
+            <div className="tab-container">
+            <div className="empty-block"></div>
+                <a className="tab-text">About Meals4U</a>
+                <a className="tab-text">Instruction</a>
+                <a className="tab-text">Incentive</a>
+                <a className="tab-text">Developers</a>
+                <div className="tab-content">
+                    <div className="empty-block"></div>
+                    <div className="tab-content-detail">
+                        <div className="about-meals4u" >
+                            Meal 4 You is an app that chooses the meal off of default selections and randomly choose a meal for the users, so users doesn't have to struggle and choose what to eat for lunch/dinner.
+                        </div>
+                        </div>
+                    <div className="tab-content-detail">
+                <h2 id="about-instruction-title" >How to Use</h2>
+                <li className="about-list">Landing Page has a default wheel for user to click play with</li>
+                <li className="about-list">Profile has user options to choose a saved wheel or add additional item onto the wheel</li>
+                <li className="about-list">You could create a customized wheel and save it to your list !</li>
+                <div className="about-instruction-visualizer">
+                    GIFs
+                    </div>
+                        </div>
+                    <div className="tab-content-detail">
+                <p>How we came up with the Idea !</p>
+                    </div>
+                    <div className="tab-content-detail">
+                    <div className="about-developers">
+                    <div className="about-developer" >
+                        <img className="developer-img" src="#" />
+                        <div className="developer-links" >
+                            
+                        </div>
+                    </div>
+
+                    <div className="about-developer" >
+                        <img className="developer-img" src="#" />
+                        <div className="developer-links" >
+                            
+                        </div>
+                    </div>
+
+                    <div className="about-developer" >
+                        <img className="developer-img" src="#" />
+                        <div className="developer-links" >
+                            
+                        </div>
+                    </div>
+
+                    <div className="about-developer" >
+                        <img className="developer-img" src="#" />
+                        <div className="developer-links" >
+                            
+                        </div>
+                    </div>
+                </div>
+                    </div>
+                </div>
+                </div>
+            {/* <div className="about-instruction">
                 <h1 className="about-title" >Instruction</h1>
                 <div className="about-instruction-details">
                     <p id="about-description" >Meal 4 You is an app that chooses the meal off of default selections and randomly choose a meal for the users, so users doesn't have to struggle and choose what to eat for lunch/dinner.</p>
                     <div id="about-instruction-steps-container" >
                         <ul id="about-instruction-steps" >
-                            <h2 id="about-instruction-title" >How to Use</h2>
-                            <li className="about-list">Landing Page has a default wheel for user to click play with</li>
-                            <li className="about-list">Profile has user options to choose a saved wheel or add additional item onto the wheel</li>
-                            <li className="about-list">You could create a customized wheel and save it to your list !</li>
                         </ul>
                     </div>
                 </div>
@@ -22,7 +76,6 @@ const AboutPage = () => {
             <div className="about-incentives-container" >
                 <div className="about-incentives" >
                     <h1 className="about-title" > Incentives </h1>
-                    <p>How we came up with the Idea !</p>
                     <div id="placeholding">
                         
                     </div>
@@ -31,36 +84,8 @@ const AboutPage = () => {
             
             <div className="about-developers-container">
                 <h1 className="about-title" >Developers</h1>
-                <div className="about-developers">
-                    <div className="about-developer" >
-                        <img className="developer-img" src="#" />
-                        <div className="developer-links" >
-                            
-                        </div>
-                    </div>
-
-                    <div className="about-developer" >
-                        <img className="developer-img" src="#" />
-                        <div className="developer-links" >
-                            
-                        </div>
-                    </div>
-
-                    <div className="about-developer" >
-                        <img className="developer-img" src="#" />
-                        <div className="developer-links" >
-                            
-                        </div>
-                    </div>
-
-                    <div className="about-developer" >
-                        <img className="developer-img" src="#" />
-                        <div className="developer-links" >
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>
+                
+            </div> */}
 
         </div>
     )
