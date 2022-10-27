4aa2e06323223c3146c1c70a4f81b4787814fd2c
Ronny Deng <ronnydeng67@gmail.com>
2022-10-27 15:36:31 -0400

    add refresh button

--- frontend/src/components/WheelIndexPage/ButtonContent.js
+++ frontend/src/components/WheelIndexPage/ButtonContent.js
@@ -1,9 +1,18 @@
 import { Popover } from "@mui/material";
 import { useState } from "react";
+import { Button } from "@mui/material";
+import './WheelIndexPage.css';
+import { useDispatch } from "react-redux";
+import { updateContents } from "../../store/contents";
+import { createWheel } from "../../store/wheels";
+import { useHistory } from "react-router-dom";
 
-const ButtonContent = (randWheel) => {
+
+
+const ButtonContent = ({ randWheel }) => {
 
     const [anchorEl, setAnchorEl] = useState(null);
+    const dispatch = useDispatch();
 
     const handleClick = (event) => {
       setAnchorEl(event.currentTarget);
@@ -16,16 +25,20 @@ const ButtonContent = (randWheel) => {
     const open = Boolean(anchorEl);
     const id = open ? 'simple-popover' : undefined;
 
-    const content = ["burger", "halal", "chinese", "ramen", "something", "something eles"]
+    const handleSaveWheel = () => {
+        dispatch(updateContents({title: randWheel.title, contents: randWheel.contents}))
+        dispatch(createWheel({title: randWheel.title, contents: randWheel.contents}))
+    }
 
     return (
-        <>
-            <button aria-describedby={id} 
+        <div className="index-button-container">
+            <Button aria-describedby={id} 
                         variant="contained" 
                         onClick={handleClick}
+                        id="google-button"
                 >
-                    Some wheel's name
-            </button>
+                    {randWheel.title}
+            </Button>
             <Popover
                 id={id}
                 open={open}
@@ -36,13 +49,18 @@ const ButtonContent = (randWheel) => {
                 horizontal: 'left',
                 }}
             >
-            <div>
-                {content.map(item => 
-                    <li key={item}>{item}</li>
-                )}
+            <div className="pop-up">
+                <div className="pop-list">
+                    {randWheel.contents.map(content => 
+                        <li key={content} id="content-list">{content}</li>
+                    )}
+                </div>
+                <div className="save-content-button">
+                    <button onClick={handleSaveWheel}>Save this wheel</button>
+                </div>
             </div>
             </Popover>
-        </>
+        </div>
     );
 }
  
