4e8f33519b92f0010b948bceb594aa16a8029668
Ronny Deng <ronnydeng67@gmail.com>
2022-11-07 21:38:15 -0500

    add button for explore

---
+++ frontend/src/components/YelpPage/YelpPage.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+const YelpPage = () => {
+
+    useEffect(() => {
+        
+    })
+
+    return (
+        <>
+        </>
+    );
+}
+ 
+export default YelpPage;
