ba46d51229c25163aeb183e52857c4bf5b882abd
Ronny Deng <ronnydeng67@gmail.com>
2022-11-08 21:24:48 -0500

    add phones

--- frontend/src/components/YelpModal/YelpRestaurant.js
+++ frontend/src/components/YelpModal/YelpRestaurant.js
@@ -1,8 +1,40 @@
+import './YelpRestaurant.css';
+import Rating from '@mui/material/Rating';
+
 const YelpRestaurant = ({ restaurant }) => {
     return (
         <div className="restaurant-container">
-            <div className="restaurant-name">
-                {restaurant.name}
+            <div className="restaurant-img">
+                <a href={restaurant.url} target='_blank' >
+                    <img src={restaurant.image_url} id="restaurant-img" /> 
+                </a>
+            </div>
+            <div className="restaurant-info">
+                <div className="restaurant-name">
+                    <a href={restaurant.url} target='_blank'>
+                        {restaurant.name}
+                    </a>
+                </div>
+                <div className="rating-count">
+                    <div className="restaurant-rating">
+                        <Rating name="read-only" 
+                            value={restaurant.rating} 
+                            readOnly 
+                            precision={0.2}
+                            />
+                    </div>
+                    <div className="review-count">
+                        {restaurant.review_count} reviews
+                    </div>
+                </div>
+                <div className="phone">
+                    Phone: {restaurant.phone}
+                </div>
+                <div className="transactions">
+                    {restaurant.transactions.map(trans => {
+                        <div key={trans}>{trans}</div>
+                    })}
+                </div>
             </div>
         </div>
     );
