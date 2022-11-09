ab56716881c885fb18b6a36d9556cfff9ce31e44
Ronny Deng <ronnydeng67@gmail.com>
2022-11-08 19:08:50 -0500

    before tried axios and add cors-anywhere

--- frontend/src/components/YelpModal/YelpModal.js
+++ frontend/src/components/YelpModal/YelpModal.js
@@ -3,6 +3,7 @@ import Modal from '@mui/material/Modal';
 import "./YelpModal.css";
 import CloseIcon from '@mui/icons-material/Close';
 import YelpList from "./YelpList";
+import axios from 'axios';
 
 const YelpModal = ({ item }) => {
 
@@ -11,10 +12,47 @@ const YelpModal = ({ item }) => {
     const [loading, setLoading] = useState(true);
     const handleOpen = () => setOpenModal(true);
     const handleClose = () => setOpenModal(false);
+
     
+    
+    // axios({
+    //     method: 'POST',
+    //     url: `${backend}api/search/`,
+    //     headers: {'Content-Type': 'application/json',
+    //             "Access-Control-Allow-Origin": "*",
+    //             "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+    //     },
+    //     withCredentials: true,
+    //     data: item
+    // })
+    // .then(response => {
+    //     setRestaurants(response.data)
+    // })
+    // .catch(err => {
+    //     console.log(err)
+    // })
+
+    let backend
+    if (document.location.hostname === 'localhost') {
+        backend = 'http://localhost:5000/'
+    } else {
+        backend = 'https://meals4yo.herokuapp.com/'
+    }
 
     useEffect(() => {
-
+        fetch(`${backend}api/search/`, {
+            method: 'post',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json'
+                
+            },
+            body: JSON.stringify({term: item})
+        })
+        .then(response => response.json())
+        .catch(err => {
+            console.log(err)
+        })
     },[])
 
     return (
