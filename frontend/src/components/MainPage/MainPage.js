b2d61ed0b29dbf804b539db1cf1efc918e9acabc
Xiangwei Liu <xiangweimg@gmail.com>
2023-01-10 12:09:56 -0500

    1.10-3

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -12,14 +12,13 @@ function MainPage() {
             <SpinWheel />
           </div>
           <div className="mainpage-bottom">
-            <div>
-              <h1>Don't know what to eat?</h1>
-              <ol>
-              <li>1. Spin the wheel.</li>
-              <li>2. We choose the food for you!</li>
-              <li>3. Please turn on your loaction to search for restaurants!</li>
-              </ol>
-              <p>Learn more about how to use <Link to="/about">here</Link></p>
+            <div className="info-container">
+                <div id="info-1" className="info-box">
+                  
+                  Spin the wheel.</div>
+                <div id="info-2" className="info-box">We choose the food for you!</div>
+                <div id="info-3" className="info-box">Please turn on your loaction to search for restaurants!</div>
+                <div id="info-4" className="info-box">Explore more? Learn more about how to use <Link to="/about">here</Link></div>
             </div>
           </div>
           <footer className="footer">
