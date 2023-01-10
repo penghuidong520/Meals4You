cc1af5c26cc0807add10a13bb8c858a8889c3ab7
Xiangwei Liu <xiangweimg@gmail.com>
2023-01-10 13:22:13 -0500

    1.10-4

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -1,6 +1,9 @@
 import Header from "../Header/Header";
 import SpinWheel from "../SpinWheel/SpinWheel";
 import { Link } from 'react-router-dom';
+import LooksOneOutlinedIcon from '@mui/icons-material/LooksOneOutlined';
+import LooksTwoOutlinedIcon from '@mui/icons-material/LooksTwoOutlined';
+import Looks3OutlinedIcon from '@mui/icons-material/Looks3Outlined';
 import './MainPage.css'; 
 
 
@@ -12,12 +15,19 @@ function MainPage() {
             <SpinWheel />
           </div>
           <div className="mainpage-bottom">
+            <div className="info-slogan">
+              <p>Easy</p><p>.</p><p>Eatery</p><p>.</p><p>Efficient</p>
+            </div>
             <div className="info-container">
                 <div id="info-1" className="info-box">
-                  
+                  <LooksOneOutlinedIcon className="info-pic" sx={{ fontSize: 50 }} />
                   Spin the wheel.</div>
-                <div id="info-2" className="info-box">We choose the food for you!</div>
-                <div id="info-3" className="info-box">Please turn on your loaction to search for restaurants!</div>
+                <div id="info-2" className="info-box">
+                  <LooksTwoOutlinedIcon className="info-pic" sx={{ fontSize: 50 }} />
+                  Food selected!</div>
+                <div id="info-3" className="info-box">
+                  <Looks3OutlinedIcon className="info-pic" sx={{ fontSize: 50 }} />
+                  Turn on your loaction to search for restaurants!</div>
                 <div id="info-4" className="info-box">Explore more? Learn more about how to use <Link to="/about">here</Link></div>
             </div>
           </div>
